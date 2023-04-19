94ec0605addfc55a571c010a643b75abe3ee29c1
Ilkin <ilkin.isgenderov91@gmail.com>
2023-04-19 18:07:11 +0400

    Sign Out module done

--- app/src/main/java/com/example/na/MainActivity.java
+++ app/src/main/java/com/example/na/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.na;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -54,5 +56,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String itemName = item.toString();
+        if(itemName.equals("Sign out")){
+            Intent intent = new Intent(this,LoginMain.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            return true;
+        }
+
+        return false;
     }
 }
